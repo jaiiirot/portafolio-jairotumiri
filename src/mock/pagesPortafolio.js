a863c93e0b9764144b625b461a04a6cf6d7eaf83
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2023-08-23 00:26:02 -0300

    mejorando la experiencia de usuario

--- src/mock/pagesPortafolio.js
+++ src/mock/pagesPortafolio.js
@@ -2,7 +2,7 @@ const hostLink =
   "https://raw.githubusercontent.com/Jairo-Tumiri/portafolio-jairotumiri/main/src/assets/pages/";
 const pagesPortafolio = [
   {
-    img: `${hostLink}ik.png`,
+    img: `${hostLink}ik.jpg`,
     name: "Indumentaria Kevin",
     link: "https://jairo-tumiri.github.io/store-ik/",
     info: "Proyecto en CoderHouse del curso de desarrollador en React JS en la plaforma de CoderHouse donde implemento conocimientos de los cursos anteriores para realizar una ecommerce con una base de datos NO-SQL y la implementacion de librerias de React JS y CSS3.",
@@ -10,23 +10,23 @@ const pagesPortafolio = [
     github: "https://github.com/Jairo-Tumiri/store-ik",
   },
   {
-    img: `${hostLink}fancy.png`,
+    img: `${hostLink}fancy.jpg`,
     name: "Fancy",
     link: "https://jairo-tumiri.github.io/ecommerce-fancy/",
     info: "Proyecto realizado en el curso de desarrollador en JavaScript de la plataforma educativa de CoderHouse. En esta propuesta el reto era implementar todo lo aprendido en el transcurso y hacerlo todo con JavaScript sin usar librerias o frameworks de javascript y profundizar con el diseño.",
     skills: "Javascript,JSON",
-    github: "https://github.com/Jairo-Tumiri/ecommerce-fancy.png",
+    github: "https://github.com/Jairo-Tumiri/ecommerce-fancy",
   },
   {
-    img: `${hostLink}codear.png`,
+    img: `${hostLink}codear.jpg`,
     name: "Codear",
     link: "https://grupocodearg.000webhostapp.com/index.php",
     info: "Una propuesta y al final siendo proyecto final para secundario tecnico en orientacion computacion. En esta propuesta hice como desarrollador Front-end y muy poco Back-end, pero mi rol enfocado fue como lider de proyecto y desarrollador Front-end.",
     skills: "PHP,HTML5,Javascript,CSS3,MySQL",
-    github: "https://github.com/Jairo-Tumiri/ecommerce-codear.png",
+    github: "https://github.com/Jairo-Tumiri/ecommerce-codear",
   },
   {
-    img: `${hostLink}et29de6.png`,
+    img: `${hostLink}et29de6.jpg`,
     name: "Esc. Tecnica N°29 DE N°6",
     link: "https://jairo-tumiri.github.io/EscuelaTecnica29/",
     info: "Proyecto de un re diseño de la pagina de una institucion secundaria. En este proyecto mi propuesta fue estructurar la pagina y asi mismo implementar cosas que fui aprendiendo en mi camino coomo developer.",
@@ -34,7 +34,7 @@ const pagesPortafolio = [
     github: "https://github.com/Jairo-Tumiri/EscuelaTecnica29",
   },
   {
-    img: `${hostLink}acatraz.png`,
+    img: `${hostLink}acatraz.jpg`,
     name: "Acatraz",
     link: "https://github.com/Jairo-Tumiri/web-acatraz",
     info: "Un proyecto planteado en el curso de Desarrollo Web en la plataforma de CoderHouse. En este proyecto el reto era implementar los conceptos que se adquirieron y reforzaron en el transcurso del curso, teniendo feedback de cada entrega del proyecto con una correccion",
