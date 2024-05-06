c4dfc43d1d5c034f63405d6917a08c3da6f5750c
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2024-05-05 22:56:31 -0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 export default function Home() {
-  // const noSkills = ["MongoDB", "Java", "C++", "MySQL", "PHP"];
-  // const Skills = skill.map((e) => {
-  //   if (e.name != noSkills.find((a) => a == e.name)) return e;
-  // });
+  const [skill, setSkill] = useState(Skills)
+  const noSkills = ["MongoDB", "Java", "C++", "MySQL", "PHP"];
+  const Skills = skill.map((e) => {
+    if (e.name != noSkills.find((a) => a == e.name)) return e;
+  });
+
   return (
     <>
-    <h1>ESTAS EN EL HOME</h1>
+       <div>
+        <h1>Home</h1>
+        <p>Home page content</p>
+        <h1>Jairo Tumiri Cuellar</h1>
+       </div>
     </>
   );
 }
