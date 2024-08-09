52884e359987949bdb7635ccd6202b710cc0a704
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2024-08-08 22:01:08 -0300

    fix: button

--- src/consts/social.ts
+++ src/consts/social.ts
@@ -1,26 +1,45 @@
 export const socialRed = [
     {
       name: "Linkedin",
-      icon: "fa-brands fa-linkedin ",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+  <path d="M8 11l0 5" />
+  <path d="M8 8l0 .01" />
+  <path d="M12 16l0 -5" />
+  <path d="M16 16v-3a2 2 0 0 0 -4 0" />
+</svg>`,
       style: "icon-redes hover:bg-[#4485edab] ",
       link: "https://www.linkedin.com/in/jairotumiri/",
     },
     {
       name: "GitHub",
-      icon: "fa-brands fa-github",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+</svg>`,
       style: "icon-redes hover:bg-[#000000ab] ",
       link: "https://github.com/jaiiirot",
     },
     {
-      name: "Instagram",
-      icon: "fa-brands fa-instagram",
-      style: " icon-redes hover:bg-[#ec4899ab] ",
-      link: "https://www.instagram.com/jaiiirot/",
+      name: "Gmail",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-at" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+  <path d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28" />
+</svg>`,
+      style: "icon-redes hover:bg-[#4485edab] ",
+      link: "mailto:jhon.jairo.tumiri@gmail.com",
     },
     {
-      name: "Twitter",
-      icon: "fa-brands fa-twitter",
-      style: " icon-redes hover:bg-[#1e80b1ab] ",
-      link: "https://twitter.com/j_jairot",
-    },
+      name: "Instagram",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+  <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+  <path d="M16.5 7.5l0 .01" />
+</svg>`,
+      style: " icon-redes hover:bg-[#ec4899ab] ",
+      link: "https://www.instagram.com/jaiiirot/",
+    }
   ];
