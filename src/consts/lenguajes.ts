68ba98df7b0aec456ee198520dee29da289da0f3
jhon jairo pastor <jhon.jairo.tumiri@gmail.com>
2025-01-31 21:00:06 -0300

    add: testimons and sliders

---
+++ src/consts/lenguajes.ts
@@ -0,0 +1,14 @@
+export const tecnologias = {
+  frontEnd: [
+    "JavaScript",
+    "React.js",
+    "CSS",
+    "HTML",
+    "Bootstrap",
+    "SCSS",
+    "Tailwind",
+    "Git",
+    "GitHub",
+  ],
+  backEnd: ["Node.js", "Express", "PHP", "C++", "MySQL", "MongoDB"],
+};
